df2ac9aa6a2f29ae005b70de35904ae9e9d57e94
Lajtaib0801 <lajtaibenja@gmail.com>
2024-05-21 16:07:40 +0200

    update

--- readme.txt
+++
@@ -1,4 +0,0 @@
-Ez a minta könyveket és könyvkiadókat tartalmaz.
-A szükséges adatbázist létre kell hozni hozzá a localhost-on a mellékelt 
-parancsfájl (mongo.bat) futtatásával
-
--- routes/routes.js
+++ routes/routes.js
@@ -1,40 +1,100 @@
 const express = require('express')
-const Film = require('../models/Film')
-const Forgalmazo = require('../models/Forgalmazo')
+const MobilGyarto = require('../models/MobilGyarto')
+const Telefon = require('../models/Telefon')
 
 const router = express.Router()
 
-router.get('/forgalmazok/:forgalmazo/filmek', async (req, res) => {
+//Post Method
+router.post('/ujGyarto', async (req, res) => {
+    const ujGyarto = new MobilGyarto({
+        _id: req.body.id,
+        nev: req.body.nev,
+        alapitva: req.body.alapitva,
+        elnok: req.body.elnok,
+    })
+
     try {
-        const forgalmazo = await Forgalmazo.findOne({ Forgalmazo: req.params.forgalmazo })
-        const filmek = await Film.find({ Studio_id: forgalmazo._id })
-        if (!filmek) {
-            res.status(404).json({ message: 'Ennek a forgalmazónak egyetlen filmje sincs.' })
-            return
-        }
-        res.status(200).json({ filmek })
-        return
+        const ujGyartoMentes = await ujGyarto.save()
+        res.status(201).json({ _id: ujGyarto._id })
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+//Post Method
+router.post('/ujMobil', async (req, res) => {
+    const ujMobil = new MobilGyarto({
+        nev: req.body.nev,
+        ar: req.body.ar,
+        szin: req.body.szin,
+        gyarto: req.body.gyarto,
+    })
+
+    try {
+        const ujTelefonMentes = await ujMobil.save()
+        res.status(201).json({ _id: ujMobil._id })
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+//Get all Method
+router.get('/getGyartok', async (req, res) => {
+    try {
+        const data = await MobilGyarto.find()
+        res.json(data)
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
 })
 
-router.get('/filmek', async (req, res) => {
+router.get('/getTelefonok', async (req, res) => {
     try {
-        let queryStr = JSON.stringify(req.query)
-        queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `$${match}`)
-        const filmek = await Film.find(JSON.parse(queryStr))
-        if (filmek.length === 0) {
-            res.status(404).json({
-                message: `${
-                    Object.values(JSON.parse(queryStr).Hossz)[0]
-                } percnél (rövidebb/hosszabb) film nincs az adatbázisban!`,
-            })
-            return
-        }
+        const data = await Telefon.find().populate('gyarto', '-_id')
+        res.json(data)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
 
-        res.status(200).json({ filmek })
-        return
+//Get by ID Method
+router.get('/:gyartoId', async (req, res) => {
+    try {
+        const data = await Telefon.find({ gyarto: req.params.gyartoId }).populate('location', '-_id')
+        if (data.length !== 0) {
+            res.json(data)
+        } else {
+            res.status(404).json({ message: 'Nincs olyan telefon az adatbázisban, amit ez a gyártó gyártott.' })
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+//Update by ID Method
+router.patch('/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const updatedData = req.body
+        const options = { new: true, runValidators: true }
+        // hogy a frissítés utáni dokumentumot kapjuk vissza
+        const result = await Telefon.findByIdAndUpdate(id, updatedData, options)
+        if (result) {
+            res.send(result)
+        } else {
+            res.status(400).json({ message: `${id} azonosítóval nem létezik telefon!` })
+        }
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+//Delete by ID Method
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const data = await Telefon.findByIdAndDelete(id)
+        res.send(`A ${data.nev} nevű telefon törölve lett.`)
     } catch (error) {
         res.status(400).json({ message: error.message })
     }
